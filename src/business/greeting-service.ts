b8b9e340234796b80a13ec6d6225656cc70debab
andrastoth021 <andrastoth021@gmail.com>
2024-10-31 14:55:32 +0100

    lesson

--- src/business/greeting-service.ts
+++ src/business/greeting-service.ts
@@ -1,8 +1,19 @@
 
+export interface ProvideNamePrimaryPort {
+  greet: (name: string) => void
+}
 
-export class GreetingService {
+export interface StoreGreetingSecondaryPort {
+  save: (greeting: string) => void
+}
+
+export class GreetingService implements ProvideNamePrimaryPort {
+
+  constructor(private readonly store: StoreGreetingSecondaryPort) {}
+
   greet(name: string) {
     const greeting = `Hello, ${name}`;
+    this.store.save(greeting);
     return greeting;
   }
 }
